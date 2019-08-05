2b73b3fa05a1d1ada4d97dd3738fd331d07d26f3
lilongtao <1132111029@qq.com>
2019-08-05 09:14:56 +0800

    修复Map结果的示例

--- src/main/java/com/llt/swaggershowdoc/SwaggerShowDocApplication.java
+++ src/main/java/com/llt/swaggershowdoc/SwaggerShowDocApplication.java
@@ -6,7 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 /**
  * @author LILONGTAO
  * Date: 2018-07-14
- * Time: 下午11：00：00
  */
 @SpringBootApplication
 public class SwaggerShowDocApplication {
